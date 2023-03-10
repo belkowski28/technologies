ca104332bc42673b96c8bb7ef67908915466fb33
lbelk <belkowski28@gmail.com>
2023-03-10 10:56:44 +0100

    csv

--- src/main/java/com/example/technologies/model/Punch.java
+++ src/main/java/com/example/technologies/model/Punch.java
@@ -12,12 +12,24 @@ public class Punch {
         this.feed = feed;
     }
 
+    private double kszt_h (){
+        return ((300*this.quantity_in_the_matrix)*60)/1000;
+    }
+
+    private double mb_kszt(){
+        //return (this.quantity_in_the_matrix/(this.feed/1000))/1000;
+        return Math.round(((this.quantity_in_the_matrix/(this.feed/1000))/1000)*1000.0)/1000.0;
+    }
+
+
     @Override
     public String toString() {
         return "Punch{" +
-                "diameter=" + diameter +
-                ", quantity_in_the_matrix=" + quantity_in_the_matrix +
-                ", feed=" + feed +
+                "diameter= " + diameter +
+                ", quantity_in_the_matrix= " + quantity_in_the_matrix +
+                ", feed= " + feed +
+                ", kszt_h= " + kszt_h() +
+                ", mb_kszt= " + mb_kszt()+
                 '}';
     }
 }
