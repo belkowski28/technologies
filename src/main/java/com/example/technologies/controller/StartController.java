7cb2b87a03db468113a9ac99958c01b28713c557
lukasz <belkowski28@gmail.com>
2023-03-12 22:25:59 +0100

    table finall

--- src/main/java/com/example/technologies/controller/StartController.java
+++ src/main/java/com/example/technologies/controller/StartController.java
@@ -1,29 +1,48 @@
 package com.example.technologies.controller;
 
+import com.example.technologies.Application;
 import com.example.technologies.model.CsvReader;
 import com.example.technologies.model.Punch;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.DoubleToIntFunction;
 
 public class StartController implements Initializable {
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private BorderPane borderPane;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        CsvReader csvReader = new CsvReader();
 
-        for(Punch punch : csvReader.getPunches()){
-            System.out.println(punch);
-        }
     }
+    @FXML
+    void speed(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/fxml/table.fxml"));
+        ScrollPane hBox = fxmlLoader.load();
+        BorderPane.setMargin(hBox,new Insets(5,5,5,5));
+        borderPane.setCenter(hBox);
+    }
+    @FXML
+    void table(ActionEvent event) throws IOException {}
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,10 @@ module com.example.technologies {
     requires javafx.fxml;
 
 
-    opens com.example.technologies to javafx.fxml;
+
     exports com.example.technologies;
     exports com.example.technologies.controller;
+    opens com.example.technologies to javafx.fxml;
+    opens com.example.technologies.model to javafx.base;
     opens com.example.technologies.controller to javafx.fxml;
 }
